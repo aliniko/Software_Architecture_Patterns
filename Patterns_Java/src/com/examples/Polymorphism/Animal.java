ebbe656e00e48a2e7dd4653ca00bfea1701a1a0b
Ali Nekoh <60889389+aliniko@users.noreply.github.com>
2023-01-18 09:16:29 +0100

    first

---
+++ Patterns_Java/src/com/examples/Polymorphism/Animal.java
@@ -0,0 +1,58 @@
+package com.examples.Polymorphism;
+
+public class Animal {
+    private String name;
+    private double height;
+    private int weight;
+    private String favFood;
+    private double speed;
+    private String sound;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getFavFood() {
+        return favFood;
+    }
+
+    public void setFavFood(String favFood) {
+        this.favFood = favFood;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public String getSound() {
+        return sound;
+    }
+
+    public void setSound(String sound) {
+        this.sound = sound;
+    }
+}
